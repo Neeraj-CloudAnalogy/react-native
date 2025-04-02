7dd01689f344ec0035434e347a68d6096b24fe8c
Neeraj200703 <neeraj@cloudanalogy.com>
2025-04-02 19:10:12 +0530

    working with redux

--- App.tsx
+++ App.tsx
@@ -1,186 +1,28 @@
-import React, { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import Home from './components/Home'; 
+import React, {createContext, useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import Home from './components/Home';
 import Profile from './components/Profile';
 import Contact from './components/Contact';
-import { Text, View, Button } from 'react-native';
+import {Text, View, Button, FlatList} from 'react-native';
 import Stacknavigator from './components/Stacknavigator';
 import DrawerComponent from './components/DrawerComponet';
 import LoginPage from './components/LoginPage';
 import OpenAndCloseDrawer from './components/OpenAndCloseDrawer';
 import ModalComponent from './components/ModalComponent';
 import SignupComponent from './components/SignupComponent';
-import axios, { Axios } from 'axios';
-
-
+import axios, {Axios} from 'axios';
+import ApiCalling from './components/ApiCalling';
+import PropsDrillingAndContextApi from './components/PropsDrillingAndContextApi';
+import PhoneList from './components/PhoneList';
 
 function App(): React.JSX.Element {
-const apiurl ='http://10.0.2.2:3000/posts'
-const [arr ,setarr] =useState([]);
-const [arr1 ,setarr1] =useState([]);
-const [arr2 ,setarr2] =useState([]);
-const [arr3 ,setarr3] =useState([]);
-
-const getreqapi =async ()=>{
-  try {
-    const response =await fetch(apiurl,{
-      method:"GET",
-      headers :{
-        "Content-Type": "application/json",
-      },
-    })
-    const data =await response.json();
-   setarr(data);
-  
-  } catch (error) {
-    console.log("error is their",error)
-  }
-  
- 
-}
-
-
-const putreqapi =async (postid:number)=>{
-  try {
-    const updatedvalue ={
-     title:"data is updated"
-    }
-    const response =await fetch(`http://10.0.2.2:3000/posts/${postid}`,{
-      method:"PUT",
-      headers :{
-        "Content-Type": "application/json",
-      },
-      body:JSON.stringify(updatedvalue)
-    })
-    const data =await response.json();
-   setarr1(data);
-  
-  } catch (error) {
-    console.log("error is their",error)
-  }
-  
- 
-}
-
-
-
-const getreqapifromaxios =async ()=>{
-  try {
-    const response =await axios.get(apiurl)
-    setarr1(response.data);
-  
-  } catch (error) {
-    console.log("error is their",error)
-  }
-  
- 
-}
-
-
-const postreq=async()=>{
-  try {
-    const data ={
-      id: 3,
-     title: "react native",
-     views: 600 
-    }
-    const response=await fetch(apiurl,{
-      method:"POST",
-      headers:{
-        "Content-Type": "application/json",
-      },
-     body:JSON.stringify(data)
-    });
-    const responseData =await response.json();
-    setarr2(responseData);
-
-
-    
-  } 
-  catch (error) {
-    console.log("error is their",error)
-  }
- 
-}
-
-const postreqbyaxios=async()=>{
-  try {
-    const data ={
-      id: 4,
-     title: "react native axioes",
-     views: 800 
-    }
-    const response=await axios.post(apiurl,data)
-    
-    setarr3(response.data);
-
-
-    
-  } 
-  catch (error) {
-    console.log("error is their",error)
-  }
- 
-}
-
   return (
-  <>
- <Text>hlo i am using api</Text>
- 
-  {arr.map((val:any)=>{
-    return (
-      <>
-    <Text>
-      {val.id}
-      {val.title}
-      {val.views}
-    </Text>
-      </>
-    )
-})
- }
-
-{arr1.map((val:any)=>{
-    return (
-      <>
-    <Text>
-      {val.id}
-      {val.title}
-      {val.views}
-    </Text>
-      </>
-    )
-})
- }
-
-{/* {arr3.map((val:any)=>{
-    return (
-      <>
-    <Text>
-      {val.id}
-      {val.title}
-      {val.views}
-    </Text>
-      </>
-    )
-})
- } */}
-   <Button title='call api from axios' onPress={ getreqapifromaxios}/>
-   <Button title='call api from fetch' onPress={ getreqapi}/>
-   <Button title='push data to server using fetch' onPress={postreq}/>
-   <Button title='push data to server using axios' onPress={postreqbyaxios}/>
-   <Button title='put data updated to server using fetch' onPress={()=>putreqapi(2)}/>
-  </>
- 
-  
-   
-    
+    <>
+      <PhoneList />
+    </>
   );
 }
-
-
-
 
 export default App;
